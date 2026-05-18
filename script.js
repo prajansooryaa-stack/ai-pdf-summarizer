120439bfa2ba6206bcffd15bead9ae14fd809fc2
prajansooryaa-stack <prajansooryaa@gmail.com>
2026-05-18 16:50:48 +0530

    Initial project upload

---
+++ script.js
@@ -0,0 +1,16 @@
+async function generateSummary() {
+
+  document.getElementById("summary").innerText =
+`• AI simulates human intelligence
+• Machine Learning is a subset of AI
+• AI is used in healthcare, finance, and education
+• Advantages include automation and accuracy
+• Disadvantages include ethical concerns and job displacement`;
+
+  document.getElementById("quiz").innerText =
+`1. What is Artificial Intelligence?
+2. What is Machine Learning?
+3. Name two applications of AI.
+4. What are advantages of AI?
+5. What are disadvantages of AI?`;
+}
